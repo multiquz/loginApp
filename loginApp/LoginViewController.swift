b6da030818805c791b061414d4e455054159ccf5
Vladimir <vproshchin@gmail.com>
2022-07-03 16:39:25 +0300

    Added alerts on Credentials mistakes (Log in button), added alerts to forgot username and password buttons Changed Background colors refactored method eraseCredentials

--- loginApp/LoginViewController.swift
+++ loginApp/LoginViewController.swift
@@ -8,17 +8,70 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet var usernameLabel: UITextField!
     @IBOutlet var passwordLabel: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeViewController = segue.destination as? WelcomeViewController else { return }
         welcomeViewController.username = usernameLabel.text
+        
     }
+    
+    @IBAction func loginButtonTapped() {
+        if usernameLabel.text != "Jeremy" || passwordLabel.text != "Password" {
+            showCredentialsAlert()
+            eraseCredentials()
+        }
+    }
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        eraseCredentials()
+    }
+    
+    @IBAction func remindUsername() {
+        let alert = UIAlertController(
+            title: "Don't you worry!",
+            message: "Your username is Jeremy",
+            preferredStyle: .alert
+        )
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true)
+        eraseCredentials()
+    }
+    
+    @IBAction func remindPassword() {
+        let alert = UIAlertController(
+            title: "Don't you worry!",
+            message: "Your password is Password",
+            preferredStyle: .alert
+        )
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true)
+        eraseCredentials()
+    }
+    
+    
+    private func showCredentialsAlert() {
+        let alert = UIAlertController(
+            title: "Wrong credentials",
+            message: "Enter correct username and/or password",
+            preferredStyle: .alert
+        )
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    private func eraseCredentials() {
+        usernameLabel.text = ""
+        passwordLabel.text = ""
+    }
+    
+    
 }
-
